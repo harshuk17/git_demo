9cbd8fba758a739968ae52c1eec4fc263349a776
harsh <harshdhiman8410@gmail.com>
2024-06-28 16:33:43 +0530

    first chnaged

---
+++ script.js
@@ -0,0 +1,20 @@
+<html>
+<body>
+
+<h2>JavaScript Variables</h2>
+
+<p>In this example, x is defined as a variable.
+Then, x is assigned the value of 6:</p>
+
+<p id="demo"></p>
+
+<script>
+let x;
+x = 6;
+let y;
+y=5;
+document.getElementById("demo").innerHTML = x;
+</script>
+
+</body>
+</html>
